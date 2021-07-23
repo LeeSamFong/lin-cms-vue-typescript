d9d375550419d97e119bdb56eb2075b9e1055ea8
LeeSamFong <597171538@qq.com>
2021-07-23 22:10:43 +0800

    chore: 添加eslint与.editorconfig配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,44 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/custom-event-name-casing': 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    'vue/experimental-script-setup-vars': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-shadow': 0,
+    'no-plusplus': 0, // 禁止使用++，--
+    'guard-for-in': 0,
+    'no-extra-semi': 0, // 和prettier冲突
+    'import/extensions': 0, // import不需要写文件扩展名
+    semi: ['error', 'never'], // 无分号
+    'import/no-unresolved': 0,
+    'no-restricted-syntax': 0,
+    'no-underscore-dangle': 0, // 无下划线
+    'consistent-return': 'off',
+    'max-len': ['error', { code: 200 }],
+    'no-use-before-define': 'off',
+    'no-prototype-builtins': 'off',
+    'class-methods-use-this': 'off',
+    'template-curly-spacing': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-param-reassign': ['error', { props: false }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['script/**/*.js'] }],
+    indent: [
+      'warn',
+      2,
+      {
+        ignoredNodes: ['TemplateLiteral'],
+        SwitchCase: 1,
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ImportDeclaration: 'never',
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
